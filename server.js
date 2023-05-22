91f3a55e7010874ae1f0de25c9f0ff187a06285f
RaiCavalcanteOliveira <raio55903@gmail.com>
2023-05-22 08:35:15 -0300

    Subir

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+io.on('connection', (socket) => {
+  console.log('Usuário conectado');
+  socket.on('chat message', (data) => io.emit('chat message', data));
+  socket.on('disconnect', () => console.log('Usuário desconectado'));
+});
+http.listen(3000, () => {
+  console.log(`Servidor rodando na porta 3000 - Link http://localhost:3000`);
+});
